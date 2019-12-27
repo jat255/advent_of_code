27c5b38878942c9594f103a595c3aa444307ea58
Joshua Taillon <joshua.taillon@nist.gov>
2019-12-26 23:32:50 -0700

    2019 day 17 part 1 completed

--- 17/puzz1.py
+++ 17/puzz1.py
@@ -1,6 +1,374 @@
 """
-"""
+--- Day 17: Set and Forget ---
+An early warning system detects an incoming solar flare and automatically
+activates the ship's electromagnetic shield. Unfortunately, this has cut off the
+Wi-Fi for many small robots that, unaware of the impending danger, are now
+trapped on exterior scaffolding on the unsafe side of the shield. To rescue
+them, you'll have to act quickly!
 
+The only tools at your disposal are some wired cameras and a small vacuum robot
+currently asleep at its charging station. The video quality is poor, but the
+vacuum robot has a needlessly bright LED that makes it easy to spot no matter
+where it is.
+
+An Intcode program, the Aft Scaffolding Control and Information Interface
+(ASCII, your puzzle input), provides access to the cameras and the vacuum robot.
+Currently, because the vacuum robot is asleep, you can only access the cameras.
+
+Running the ASCII program on your Intcode computer will provide the current view
+of the scaffolds. This is output, purely coincidentally, as ASCII code: 35 means
+#, 46 means ., 10 starts a new line of output below the current one, and so on.
+(Within a line, characters are drawn left-to-right.)
+
+In the camera output, # represents a scaffold and . represents open space. The
+vacuum robot is visible as ^, v, <, or > depending on whether it is facing up,
+down, left, or right respectively. When drawn like this, the vacuum robot is
+always on a scaffold; if the vacuum robot ever walks off of a scaffold and
+begins tumbling through space uncontrollably, it will instead be visible as X.
+
+In general, the scaffold forms a path, but it sometimes loops back onto itself.
+For example, suppose you can see the following view from the cameras:
+
+..#..........
+..#..........
+#######...###
+#.#...#...#.#
+#############
+..#...#...#..
+..#####...^..
+
+Here, the vacuum robot, ^ is facing up and sitting at one end of the scaffold
+near the bottom-right of the image. The scaffold continues up, loops across
+itself several times, and ends at the top-left of the image.
+
+The first step is to calibrate the cameras by getting the alignment parameters
+of some well-defined points. Locate all scaffold intersections; for each, its
+alignment parameter is the distance between its left edge and the left edge of
+the view multiplied by the distance between its top edge and the top edge of the
+view. Here, the intersections from the above image are marked O:
+
+..#..........
+..#..........
+##O####...###
+#.#...#...#.#
+##O###O###O##
+..#...#...#..
+..#####...^..
+
+For these intersections:
+
+- The top-left intersection is 2 units from the left of the image and 2 units
+  from the top of the image, so its alignment parameter is 2 * 2 = 4.
+- The bottom-left intersection is 2 units from the left and 4 units from the
+  top, so its alignment parameter is 2 * 4 = 8.
+- The bottom-middle intersection is 6 from the left and 4 from the top, so its
+  alignment parameter is 24.
+- The bottom-right intersection's alignment parameter is 40.
+
+To calibrate the cameras, you need the sum of the alignment parameters. In the
+above example, this is 76.
+
+Run your ASCII program. What is the sum of the alignment parameters for the
+scaffold intersections?
+
+"""
+import itertools
+from matplotlib import pyplot as plt
+import numpy as np
+import matplotlib.animation as animation
+import random
+
+class Computer():
+    def __init__(self, name, inp, 
+                 phase_setting=None, input_code=None, debug_level='off'):
+        self.name = name
+        self.mem = [int(i) for i in inp.split(',')] + \
+            [0]*len(inp.split(','))*100
+        self.input_stack = []
+        if phase_setting is not None:
+            self.input_stack += [phase_setting]
+        if input_code is not None:
+            self.input_stack += [input_code]
+        self.rel_base = 0
+        self.output = None
+        self.output_pos = None
+        self.cur_opcode = None
+        self.total_output = []
+        self.this_runs_output = []
+        self.debug_level = debug_level
+
+
+    def parse_param_modes(self, param_int, nparams):
+        params = [0] * nparams
+        if param_int:
+            param_str = f'{param_int:0{nparams}g}'
+        else:
+            param_str = '0' * nparams
+        for i, mode in enumerate(param_str[::-1]):
+            param_val = self.mem[self.pos + i + 1]
+            if int(mode) == 0:
+                # position mode, so value is position in memory of parameter
+                if i < 2 and self.cur_opcode != 3:
+                    params[i] = self.mem[param_val]
+                else:
+                    # unless it's the output value, then it's just the position
+                    params[i] = param_val
+            elif int(mode) == 1:
+                # immediate mode, so value is just the parameter
+                params[i] = param_val
+            elif int(mode) == 2:
+                # relative mode (similar to position, but with rel_base added)
+                # for opcode 3, since we're writing, we want to be in immediate 
+                # mode:
+                if i < 2:
+                    params[i] = self.mem[param_val + self.rel_base]
+                else:
+                    # unless it's the output value, then it's just the position 
+                    # (this is immediate mode)
+                    params[i] = param_val + self.rel_base
+            else:
+                raise ValueError('Parameter mode should be either 0 or 1')
+        for p in params: self.dbg_print(str(p), space=' ')
+        self.dbg_print('\t' * (1 if nparams==1 else 0) + '| ')
+        return params
+
+    
+    def print_mem_array(self):
+        print('indx: ', end='')
+        for num in range(len(self.mem)):
+            print(f'{num:4g} ', end='')
+        print('')
+        print('vals: ', end='')
+        for num in self.mem[:100]:
+            print(f'{num:4g} ', end='')
+        print('')
+
+    
+    def dbg_print(self, string, space='\t', newline=False):
+        if self.debug_level == 'all':
+            print(space + string,
+                  end='' if not newline else '\n')
+        if self.debug_level == 'output':
+            if self.cur_opcode in [3, 4]:
+                print(space + string,
+                    end='' if not newline else '\n')
+
+
+    def run_intcode(self, new_input=None):
+        """
+        inp : str
+            The comma-separated string of inputs
+        """
+        if not hasattr(self, 'pos'):
+            self.pos = 0
+        if not hasattr(self, 'iteration'):
+            self.iteration = 1
+        if not hasattr(self, 'last_output'):
+            self.last_output = None
+        self.this_runs_output = []
+        
+        # opcode 1 means add; opcode 2 means multiply
+        # next three numbers are first input index, second input index, and index to
+        # write to
+        
+        # opcode 3 means take an input and save it to position
+        # opcode 4 means output (print) the parameter
+        while True:
+            opcode = self.mem[self.pos]
+            param_int = None
+            params = None
+            jumped = False
+
+            if opcode == 99:
+                print('REACHED OPCODE 99 -- BREAKING')
+                return False
+
+            if opcode > 9:
+                # we have more than two digits, so split opcode and parameters
+                reduced_opcode = int(str(opcode)[-2:])
+                param_int = int(str(opcode)[:-2])
+                opcode = reduced_opcode
+
+            self.cur_opcode = opcode
+
+            self.dbg_print(f'({self.pos:04g})', space='\n')
+            self.dbg_print(f'{opcode}')
+
+            if opcode == 1:
+                # add values of first two parameters and store in third param
+                nparams = 3
+                params = self.parse_param_modes(param_int, nparams)
+                self.dbg_print(f'ADD ({nparams})')
+                self.mem[params[2]] = params[0] + params[1]
+                self.dbg_print(f'\t\t| {params[0]} + {params[1]} -> mem[{params[2]}]')
+            elif opcode == 2:
+                # multiply values of first two parameters and store in third param
+                nparams = 3
+                params = self.parse_param_modes(param_int, nparams)
+                self.dbg_print(f'MULT ({nparams})')
+                self.mem[params[2]] = params[0] * params[1]
+                self.dbg_print(f'\t| {params[0]} * {params[1]} -> mem[{params[2]}]')
+            elif opcode == 3:
+                # take input from user and store at postion of only parameter
+                nparams = 1
+                params = self.parse_param_modes(param_int, nparams)
+                self.dbg_print(f'INPUT ({nparams})')
+                if len(self.input_stack) > 0:
+                    input_code = int(self.input_stack.pop(0))
+                    self.dbg_print(f'\t| got {input_code} from input_stack')
+                else:
+                    if new_input is not None:
+                        input_code = int(new_input)
+                        self.dbg_print(f'\t| got {input_code} from new_input')
+                        # make sure to clear new_input, so we don't think we
+                        # got another input the next time we see an opcode 3                        
+                        new_input = None
+                    # we've run out of inputs, so we have to pause and wait
+                    # for the program to continue with a new input
+                    # Return None to indicate that we're not done yet
+                    else:
+                        self.output = self.last_output
+                        self.dbg_print('\t| no new input; pausing', 
+                                       newline=True)
+                        return None
+                self.mem[params[0]] = input_code
+                self.dbg_print(f'| {input_code} -> mem[{params[0]}]')
+            elif opcode == 4:
+                # output value of the only parameter
+                nparams = 1
+                params = self.parse_param_modes(param_int, nparams)
+                self.dbg_print(f'OUTPUT ({nparams})')
+                self.last_output = params[0]
+                self.output_pos = self.pos
+                self.output = self.last_output
+                self.total_output += [self.last_output]
+                self.this_runs_output += [self.last_output]
+                self.dbg_print(f'\t* **OUTPUT** {self.output}')
+            elif opcode == 5:
+                nparams = 2
+                params = self.parse_param_modes(param_int, nparams)
+                self.dbg_print(f'JMP_IF_NONZERO ({nparams})')
+                if params[0] != 0:
+                    self.pos = params[1]
+                    jumped = True
+                    self.dbg_print(f'| {params[0]} != 0, so jumped | {params[1]} -> pos')
+                else:
+                    self.dbg_print(f'| {params[0]} == 0, so did not jump')
+            elif opcode == 6:
+                nparams = 2
+                params = self.parse_param_modes(param_int, nparams)
+                self.dbg_print(f'JMP_IF_ZERO ({nparams})')
+                if params[0] == 0:
+                    self.pos = params[1]
+                    jumped = True
+                    self.dbg_print(f'\t| {params[0]} == 0, so jumped | {params[1]} -> pos')
+                else:
+                    self.dbg_print(f'\t| {params[0]} != 0, so did not jump')
+
+            elif opcode == 7:
+                nparams = 3
+                params = self.parse_param_modes(param_int, nparams)
+                self.dbg_print(f'LESS_THAN ({nparams})')
+                if params[0] < params[1]:
+                    self.mem[params[2]] = 1
+                    self.dbg_print(f'\t| {params[0]} < {params[1]}, so 1 -> {params[2]}')
+                else:
+                    self.mem[params[2]] = 0
+                    self.dbg_print(f'\t| {params[0]} !< {params[1]}, so 0 -> {params[2]}')
+            elif opcode == 8:
+                nparams = 3
+                params = self.parse_param_modes(param_int, nparams)
+                self.dbg_print(f'EQUALS ({nparams})')
+                if params[0] == params[1]:
+                    self.mem[params[2]] = 1
+                    self.dbg_print(f'\t| was_equal | 1 -> mem[{params[2]}]')
+                else:
+                    self.mem[params[2]] = 0
+                    self.dbg_print(f'\t| not_equal | 0 -> mem[{params[2]}]')
+            elif opcode == 9:
+                nparams = 1
+                params = self.parse_param_modes(param_int, nparams)
+                self.dbg_print('ADJ_RELBASE')
+                self.dbg_print(f'\t| {self.rel_base} += {params[0]}')
+                self.rel_base += params[0]
+                self.dbg_print(f' = {self.rel_base}', space='')
+                
+            
+            if not jumped:
+                self.pos += nparams + 1
+            
+            self.iteration += 1
+
+        self.output = self.last_output
+
+with open('17/input', 'r') as f:
+    inp = f.readline()
+
+c = Computer('maze', inp, debug_level='off')
+
+def print_output(output):
+    print(''.join([chr(o) for o in output]))
+
+def scaffold_to_numpy(scaffold, dtype=int):
+    if dtype == int:
+        lines = ' '.join([str(o) for o in scaffold]).strip(' 10').split('10')
+        lines = [l.strip().split(' ') for l in lines]
+        lines = [[int(i) for i in l] for l in lines]
+        arr = np.array(lines, dtype=int)
+   
+    elif dtype == str:
+        lines = ''.join([chr(o) for o in scaffold]).strip('\n').split('\n')
+        lines = [list(l) for l in lines]
+        arr = np.array(lines, dtype=str)
+    
+    return arr
+
+def test_intersection(arr, index, dtype=int):
+    y = index[0]
+    x = index[1]
+    val_to_test = 35 if dtype==int else '#'
+    
+    if y == 0 or x == 0 or y == arr.shape[0] - 1 or x == arr.shape[1] - 1:
+        return False
+
+    # to be an intersection, all four (up, down, left right) values must be
+    # 35 (#)
+    if arr[y+1, x] == val_to_test and arr[y-1, x] == val_to_test and \
+        arr[y, x+1] == val_to_test and arr[y, x-1] == val_to_test and \
+            arr[y, x] == val_to_test:
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
+    testing = False
 
+    if testing:
+        lines = ['..#..........',
+                 '..#..........',
+                 '#######...###',
+                 '#.#...#...#.#',
+                 '#############',
+                 '..#...#...#..',
+                 '..#####...^..']
+        lines = [list(l) for l in lines]
+        lines = [[ord(i) for i in l] for l in lines]
+        arr = np.array(lines, dtype=int)
+    else:
+        c.run_intcode()
+        print_output(c.this_runs_output)
+        arr = scaffold_to_numpy(c.this_runs_output)
+    intersections = []
+    for index, x in np.ndenumerate(arr):
+        if test_intersection(arr, index): intersections.append(index)
+    print(intersections)
+
+    total = 0
+    for i in intersections:
+        total += i[0]*i[1]
+    
+    print(f'Total alignment parameter is {total}')
+    # plt.imshow(arr)
+    # plt.show()
+    
+    # Answer is 4688
